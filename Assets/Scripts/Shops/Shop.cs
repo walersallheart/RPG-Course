83ca4cf6fee4e7cff45d806499d742a838e6307b
finalmegaman NA <finalmegaman@gmail.com>
2021-06-24 18:50:48 -0700

    12 Configuring Stock and Price.

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -10,20 +10,26 @@ namespace RPG.Shops{
     {
         [SerializeField] string shopName;
 
+        //Stock Config
+        [SerializeField] StockItemConfig[] stockConfig;
+
+        [System.Serializable]
+        class StockItemConfig{
+            public InventoryItem item;
+            public int initialStock;
+            [Range(0,100)]
+            public float buyingDiscountPercentage;
+        }
+
         public event Action onChange;
 
         public IEnumerable<ShopItem> GetFilteredItems(){ 
-            yield return new ShopItem(InventoryItem.GetFromID("e75a0c32-d41c-4651-8496-92cb958a8f1e"),
-            10, 10.0f, 0);
+            foreach (StockItemConfig config in stockConfig)
+            {
+                float price = config.item.GetPrice() * (1 - config.buyingDiscountPercentage/100);
 
-            yield return new ShopItem(InventoryItem.GetFromID("28c6f2e6-46e9-4879-a14f-d6998c781cb7"),
-            10, 10099.0f, 0);
-
-            yield return new ShopItem(InventoryItem.GetFromID("dbc1e40e-d3bd-4e26-a62b-6cff0e46c415"),
-            10, 10.0f, 0);
-
-            yield return new ShopItem(InventoryItem.GetFromID("77ad666c-513c-4c88-87b2-c8594bcd5a89"),
-            10, 10.0f, 0);
+                yield return new ShopItem(config.item, config.initialStock, price, 0);
+            }
         }
         public void SelectFilter(ItemCategory category){}
         public ItemCategory GetFilters(){ return ItemCategory.None; }
