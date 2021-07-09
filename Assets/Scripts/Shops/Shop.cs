300e89cef6f2cd2d839d619a273a288696f136e3
finalmegaman NA <finalmegaman@gmail.com>
2021-07-08 18:27:48 -0700

    21 Checking Inventory Space.

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -63,8 +63,45 @@ namespace RPG.Shops{
         public bool CanTransact() {
             if (IsTransactionEmpty()) { return false; }
             if (!HasSufficientFunds()) { return false; }
+            if (!HasInventorySpace()) { return false; }
 
             return true;
+        }
+
+        public bool HasSufficientFunds()
+        {
+            Purse purse = currentShopper.GetComponent<Purse>();
+
+            if (purse == null) { return false; }
+
+            return purse.GetBalance() >= TransactionTotal();
+        }
+
+        public bool IsTransactionEmpty()
+        {
+            return transaction.Count == 0;
+        }
+
+        public bool HasInventorySpace()
+        {
+            Inventory shopperInventory = currentShopper.GetComponent<Inventory>();
+
+            if (shopperInventory == null) { return false; }
+
+            List<InventoryItem> flatItems = new List<InventoryItem>();
+
+            foreach (ShopItem shopItem in GetAllItems())
+            {
+                InventoryItem item = shopItem.GetInventoryItem();
+                int quantity = shopItem.GetQuantityInTransaction();
+
+                for (int i = 0; i < quantity; i++)
+                {
+                    flatItems.Add(item);
+                }
+            }
+
+            return shopperInventory.HasSpaceFor(flatItems);
         }
 
         public void ConfirmTransaction(){
@@ -144,20 +181,6 @@ namespace RPG.Shops{
             }
 
             return true;
-        }
-
-        public bool HasSufficientFunds()
-        {
-            Purse purse = currentShopper.GetComponent<Purse>();
-
-            if (purse == null) { return false; }
-
-            return purse.GetBalance() >= TransactionTotal();
-        }
-
-        private bool IsTransactionEmpty()
-        {
-            return transaction.Count == 0;
         }
     }
 }
