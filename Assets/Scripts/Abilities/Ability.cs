2562914e0a8bdc87f18f6b29e27440fa3419820d
finalmegaman NA <finalmegaman@gmail.com>
2021-07-22 18:50:54 -0700

    34 Triggering the Action Bar.

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,10 @@
+using GameDevTV.Inventories;
+using UnityEngine;
+
+namespace RPG.Abilities{
+    [CreateAssetMenu(fileName ="My Ability", menuName = "Abilities/Ability", order = 0)]
+    public class Ability : ActionItem
+    {
+        
+    }
+}
