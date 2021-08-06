4eae62e6d0fb69fcd9db13243b58ae3147880898
finalmegaman NA <finalmegaman@gmail.com>
2021-08-05 18:49:13 -0700

    40 Filtering Strategy Pattern.

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -7,13 +7,19 @@ namespace RPG.Abilities{
     public class Ability : ActionItem
     {
         [SerializeField] TargetingStrategy targetingStrategy;
-        
+        [SerializeField] FilterStrategy[] filterStrategies;
+
         public override void Use(GameObject user){
             targetingStrategy.StartTargeting(user, TargetAcquired);
         }
 
         private void TargetAcquired(IEnumerable<GameObject> targets){
             Debug.Log("Target Acquired");
+
+            foreach (var filterStrategy in filterStrategies)
+            {
+                targets = filterStrategy.Filter(targets);
+            }
 
             foreach (var target in targets)
             {
